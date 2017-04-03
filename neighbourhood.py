1843492bbbbafa32d9b5548e6e2a4d31a805e090
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-03 13:20:01 +0200

    Add module with neighbourhood function kNN

---
+++ neighbourhood.py
@@ -0,0 +1,50 @@
+from operator import itemgetter
+from similarity import pearson
+
+
+"""knn finds the k-nearest neighbours of the user
+supplied. 
+
+Parameters:
+    user := the current user we are building the knn list for
+    utility_matrix := the matrix with the realtion for user/items
+    k := the number of nearest neighbours returned
+    default is 5
+
+Returns:
+    Return a tuple with:
+        knn list
+        max similarity value
+        min similarity value 
+
+"""
+def knn(user,utility_matrix,k=5):
+    
+    user_ratings = utility_matrix[user,:]
+
+    neighbours = []
+    max_sim = -100
+    min_sim = 100
+
+    #find similarity to all other users
+    for i in range(len(utility_matrix)):
+        #skip if current user
+        if i == user:
+            continue
+
+        other_user_ratings = utility_matrix[i,:]
+        sim = pearson(user_ratings,other_user_ratings)
+        
+        min_sim = min(min_sim,sim[0])
+        max_sim = max(max_sim,sim[0])
+        
+        neighbours.append((i,sim[0]))
+
+    #Optimization as per 2nd answer: 
+    #http://stackoverflow.com/questions/10695139/sort-a-list-of-tuples-by-2nd-item-integer-value
+
+    neighbours = sorted(neighbours, key=itemgetter(1))
+    neighbours.reverse()
+
+    knn = neighbours[:k]
+    return  (knn,max_sim,min_sim)
