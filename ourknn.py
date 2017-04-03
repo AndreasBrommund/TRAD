de3e4776d76176e4ec453cf9470b6aec470208e7
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-03 09:08:48 +0200

    Update main program to use pearson similarity

--- ourknn.py
+++ ourknn.py
@@ -1,5 +1,6 @@
 import pandas
 import numpy as np
+from similarity import pearson
 from scipy.stats.mstats import pearsonr
 
 data_file = "100kratings.csv" 
@@ -16,23 +17,16 @@ cols,col_pos = np.unique(data[:,1],return_inverse=True)
 utility_matrix = np.zeros((len(rows),len(cols)),dtype=data.dtype)
 utility_matrix[row_pos,col_pos] = data[:,2]
 
-
+#testing with the first two user rows.
 row1 = utility_matrix[0,:]
 row2 = utility_matrix[1,:]
 
-print(row1)
-print(row2)
-res1 = []
-res2 = []
+sim = pearson(row1,row2)
 
-for i in range(len(row1)):
-    if row1[i] != 0.0 and row2[i] != 0.0:
-        res1.append(row1[i])
-        res2.append(row2[i])
-
-print(res1)
-print(res2)
+print("The similarity is:",sim[0])
 
 
-sim = pearsonr(res2,res1)
-print("Similarity",sim[0])
+
+
+
+
