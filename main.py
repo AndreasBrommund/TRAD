d3a6ea9d2284447d6571a78b87222beea54f6ecb
Andreas Brommund <andreas@brommund.se>
2017-04-10 09:38:41 +0200

    Make a collaborative funk

--- main.py
+++ main.py
@@ -17,88 +17,94 @@ def main():
     population_size = len(np.unique(dataset.values[:,0]))
     users = generate_user_sample(population_size,sample_size)
     
-    list_precisions = []
-    list_recalls = []
-    total_hits = 0
-    total_recommendations = 0
-    total_good_films = 0
+    list_precisions_collaborative = []
+    list_recalls_collaborative = []
+    total_hits_collaborative = 0
+    total_recommendations_collaborative = 0
+    total_good_films_collaborative = 0
      
     #Get all umatrix and all the mapings
-    print("Start", file=sys.stderr) 
+    print("Start: build umatrix", file=sys.stderr) 
     umatrix,movie_to_matrix,matrix_to_movie,user_to_matrix,matrix_to_user = matrix.build_matrix(dataset)
-    print("End", file=sys.stderr)
+    print("End: build umatrix", file=sys.stderr)
 
     for i,user in enumerate(users):
-
-
-        
+ 
         print("User ",i, " of ", len(users),file=sys.stderr)
 
         traning, test = generate_traning_and_test(user,dataset) 
-
         matrix.remove(test,umatrix,user_to_matrix,movie_to_matrix)
-        
-        #Get knn
-        sim_users,unsim_users,max_sim,min_sim = knn(user_to_matrix[user],umatrix)
         
         #Sanity check
         for f in test['movieId']:
             assert umatrix[user_to_matrix[user],movie_to_matrix[f]] == 0, "Test data was not removed correctly"
 
-        print("Num of sim users: ",len(sim_users)," Num of unsim users: ",len(unsim_users),file=sys.stderr)
-        print("Max: ",max_sim," Min: ",min_sim,file=sys.stderr)
+       
 
-        predictions = predict(test,umatrix,movie_to_matrix,sim_users,unsim_users)
-
-        if len(predictions) == 0:
-            list_precisions.append(0)
-            list_recalls.append(0)
-            total_good_films += len(test[(test.rating > 3)])
-            matrix.add(test,umatrix,user_to_matrix,movie_to_matrix)
-
-            continue
-
-        film_recommendation = recommendations(test,predictions)
+        #Collaborative part
+        precision,recall,hits,recommended_films,good_films = collaborative(user,umatrix,movie_to_matrix,user_to_matrix,traning,test)
         
-        if len(film_recommendation) == 0:
-            list_precisions.append(0)
-            list_recalls.append(0)
-            total_good_films += len(test[(test.rating > 3)])
-            
-            matrix.add(test,umatrix,user_to_matrix,movie_to_matrix)
+        list_precisions_collaborative.append(precision)
+        list_recalls_collaborative.append(recall)
+        total_hits_collaborative += hits
+        total_recommendations_collaborative += recommended_films
+        total_good_films_collaborative += good_films
 
-            continue
+        #Content part
 
-        precision,recall,hits,recommended_films,good_films = calculate_hit_rate(test,film_recommendation) 
-
-        list_precisions.append(precision)
-        list_recalls.append(recall)
-        total_hits += hits
-        total_recommendations += recommended_films
-        total_good_films += good_films
+        #Hybrid part
 
         
         matrix.add(test,umatrix,user_to_matrix,movie_to_matrix)
 
-    print("Precisions\n",list_precisions)
-    print("Recalls\n",list_recalls)
-    print("Total hits: ",total_hits)
-    print("Total recommendations: ",total_recommendations)
-    print("Total good films: ",total_good_films)
+
+    print("Result collaborative:")
+
+    print("Precisions\n",list_precisions_collaborative)
+    print("Recalls\n",list_recalls_collaborative)
+    print("Total hits: ",total_hits_collaborative)
+    print("Total recommendations: ",total_recommendations_collaborative)
+    print("Total good films: ",total_good_films_collaborative)
 
     print("\nPredictions")
-    print("Mean: ",np.mean(list_precisions))
-    print("Median: ",np.median(list_precisions))
-    print("Overall: ",total_hits/total_recommendations)
+    print("Mean: ",np.mean(list_precisions_collaborative))
+    print("Median: ",np.median(list_precisions_collaborative))
+    print("Overall: ",total_hits_collaborative/total_recommendations_collaborative)
         
     print("\nRecall")
-    print("Mean: ",np.mean(list_recalls))
-    print("Median: ",np.median(list_recalls))
-    print("Overall: ",total_hits/total_good_films)
+    print("Mean: ",np.mean(list_recalls_collaborative))
+    print("Median: ",np.median(list_recalls_collaborative))
+    print("Overall: ",total_hits_collaborative/total_good_films_collaborative)
 
     print("\nPopulation size: ",population_size)
     print("Sample size: ",sample_size)
 
+
+
+
+
+
+#Return: Precision, Recal, hits, recommended_films, good_films
+def collaborative(user,umatrix,movie_to_matrix,user_to_matrix,traning,test):
+    #Get knn
+    sim_users,unsim_users,max_sim,min_sim = knn(user_to_matrix[user],umatrix)
+
+    print("Num of sim users: ",len(sim_users)," Num of unsim users: ",len(unsim_users),file=sys.stderr)
+    print("Max: ",max_sim," Min: ",min_sim,file=sys.stderr)
+
+    predictions = predict(test,umatrix,movie_to_matrix,sim_users,unsim_users)
+
+    if len(predictions) == 0:
+        #Precision, Recal, hits, recommended_films, good_films
+        return 0,0,0,0,len(test[(test.rating > 3)])
+        
+    film_recommendation = recommendations(test,predictions)
+
+    if len(film_recommendation) == 0:
+        #Precision, Recal, hits, recommended_films, good_films    
+        return 0,0,0,0,len(test[(test.rating > 3)])
+
+    return calculate_hit_rate(test,film_recommendation) 
 
 def read_csv(path):
     return pandas.read_csv(path,
