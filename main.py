6ea90471d951a1100cdf7e7e6809b803a08324fb
Andreas Brommund <andreas@brommund.se>
2017-04-10 13:25:45 +0200

    Add content part

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ import matrix
 from operator import itemgetter
 from sklearn.model_selection import train_test_split
 import sys
+from sklearn.ensemble import RandomForestClassifier
 
 def main():
     url_to_data_file = argv[1]
-    sample_size = int(argv[2])
+    url_to_film_file = argv[2]
+    sample_size = int(argv[3])
+
+    #read only the films genre
+    film_dataset = pandas.read_csv(url_to_film_file,sep = "|",usecols=range(5,24))
+    
 
     dataset = read_csv(url_to_data_file)
     population_size = len(np.unique(dataset.values[:,0]))
@@ -22,7 +28,15 @@ def main():
     total_hits_collaborative = 0
     total_recommendations_collaborative = 0
     total_good_films_collaborative = 0
-     
+    
+
+    list_precisions_content = []
+    list_recalls_content = []
+    total_hits_content = 0
+    total_recommendations_content = 0
+    total_good_films_content = 0
+
+
     #Get all umatrix and all the mapings
     print("Start: build umatrix", file=sys.stderr) 
     umatrix,movie_to_matrix,matrix_to_movie,user_to_matrix,matrix_to_user = matrix.build_matrix(dataset)
@@ -32,7 +46,7 @@ def main():
  
         print("User ",i, " of ", len(users),file=sys.stderr)
 
-        traning, test = generate_traning_and_test(user,dataset) 
+        training, test = generate_traning_and_test(user,dataset) 
         matrix.remove(test,umatrix,user_to_matrix,movie_to_matrix)
         
         #Sanity check
@@ -42,7 +56,7 @@ def main():
        
 
         #Collaborative part
-        precision,recall,hits,recommended_films,good_films = collaborative(user,umatrix,movie_to_matrix,user_to_matrix,traning,test)
+        precision,recall,hits,recommended_films,good_films = collaborative(user,umatrix,movie_to_matrix,user_to_matrix,training,test)
         
         list_precisions_collaborative.append(precision)
         list_recalls_collaborative.append(recall)
@@ -52,16 +66,30 @@ def main():
 
         #Content part
 
+        precision,recall,hits,recommended_films,good_films = content_predict(user,training,test,film_dataset,3,200)
+                    
+        list_precisions_content.append(precision)
+        list_recalls_content.append(recall)
+        total_hits_content += hits
+        total_recommendations_content += recommended_films
+        total_good_films_content += good_films
+
         #Hybrid part
 
-        
+
+        #Clean 
         matrix.add(test,umatrix,user_to_matrix,movie_to_matrix)
 
+    print("Raw data collaborative\n")
+    print("Precisions\n",list_precisions_collaborative)
+    print("Recalls\n",list_recalls_collaborative)
+
+    print("Raw data content\n")
+    print("Precisions\n",list_precisions_content)
+    print("Recalls\n",list_recalls_content)
 
     print("Result collaborative:")
 
-    print("Precisions\n",list_precisions_collaborative)
-    print("Recalls\n",list_recalls_collaborative)
     print("Total hits: ",total_hits_collaborative)
     print("Total recommendations: ",total_recommendations_collaborative)
     print("Total good films: ",total_good_films_collaborative)
@@ -75,6 +103,22 @@ def main():
     print("Mean: ",np.mean(list_recalls_collaborative))
     print("Median: ",np.median(list_recalls_collaborative))
     print("Overall: ",total_hits_collaborative/total_good_films_collaborative)
+
+    print("Result content:")
+
+    print("Total hits: ",total_hits_content)
+    print("Total recommendations: ",total_recommendations_content)
+    print("Total good films: ",total_good_films_content)
+
+    print("\nPredictions")
+    print("Mean: ",np.mean(list_precisions_content))
+    print("Median: ",np.median(list_precisions_content))
+    print("Overall: ",total_hits_content/total_recommendations_content)
+        
+    print("\nRecall")
+    print("Mean: ",np.mean(list_recalls_content))
+    print("Median: ",np.median(list_recalls_content))
+    print("Overall: ",total_hits_content/total_good_films_content)
 
     print("\nPopulation size: ",population_size)
     print("Sample size: ",sample_size)
@@ -98,13 +142,44 @@ def collaborative(user,umatrix,movie_to_matrix,user_to_matrix,traning,test):
         #Precision, Recal, hits, recommended_films, good_films
         return 0,0,0,0,len(test[(test.rating > 3)])
         
-    film_recommendation = recommendations(test,predictions)
+    return calculate_hit_rate(test,predictions) 
 
-    if len(film_recommendation) == 0:
-        #Precision, Recal, hits, recommended_films, good_films    
+#Return: Precision, Recal, hits, recommended_films, good_films
+def content_predict(user,training,test,film_dataset,depth,trees): 
+
+    film_ratings_training = []
+    film_atribute_training = []
+    for index, row in training[(training.userId == user)].iterrows():
+        movie_id = int(row['movieId'])-1
+        rating = row['rating'] 
+        if rating > 0:
+            film_ratings_training.append(rating)
+            film_atribute_training.append(film_dataset.values[movie_id])        
+
+    film_id_test = []
+    film_atribute_test = []
+    for index, row in test[(test.userId == user)].iterrows():
+        movie_id = int(row['movieId'])-1
+        rating = row['rating'] 
+        if rating > 0:
+            film_atribute_test.append(film_dataset.values[movie_id])  
+            film_id_test.append(movie_id+1)
+
+    rf = RandomForestClassifier(max_depth = depth,n_estimators = trees)
+    rf.fit(film_atribute_training,film_ratings_training)
+        
+    ratings = rf.predict(film_atribute_test)
+ 
+    res = []
+
+    for index,value in enumerate(ratings):
+        if value > 3:
+            res.append((film_id_test[index],1))
+
+    if len(res) == 0:
         return 0,0,0,0,len(test[(test.rating > 3)])
 
-    return calculate_hit_rate(test,film_recommendation) 
+    return calculate_hit_rate(test,res) 
 
 def read_csv(path):
     return pandas.read_csv(path,
@@ -127,17 +202,7 @@ def generate_traning_and_test(user,dataset):
     return traning_data,user_rating_test
     
     
-def recommendations(test,predictions):
-    """good_films = test[(test.rating > 3)]
 
-    films_to_recomend = []
-    if  len(good_films) < 5: 
-        films_to_recommend = predictions[:len(good_films) ] 
-    else:   
-        films_to_recommend = predictions[:5]
-    """
-
-    return predictions
 
 def calculate_hit_rate(test,films_to_recommend):
     good_films = test[(test.rating > 3)]
