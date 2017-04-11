e50d8cbde7144d40fb2d4f305ccc4d588d18b0c5
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-11 14:11:24 +0200

    Add working toplist

--- toprated.py
+++ toprated.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy as np
 from sys import argv
 
 file_path = argv[1]
@@ -6,28 +7,17 @@ film_name_path = argv[2]
 old = argv[3]
 sep = argv[4]
 
-if old == "1":
-    old = True
-else:
-    old = False
-
 #source: http://www.gregreda.com/2013/10/26/using-pandas-on-the-movielens-dataset/
-movies = None
-
-if old:
-    movies = pandas.read_csv(film_name_path, sep='|', usecols=range(5),encoding='latin-1')
-else:
-    movies = pandas.read_csv(film_name_path,sep=sep)
-
-
 ratings = pandas.read_csv(file_path,sep=sep)
-movie_ratings = pandas.merge(movies, ratings)
+movies = pandas.read_csv(film_name_path,sep='|',usecols=range(5))
+df = pandas.merge(ratings,movies)
 
-most_rated = []
-if old:
-    most_rated = movie_ratings['name'].value_counts()[:5]
-else:
-    most_rated = movie_ratings['title'].value_counts()[:5]
+group = df.groupby(['name','movieId'])['rating'].sum().reset_index()
+sort = group.sort_values('rating',ascending=False)
+print(sort[:10])
 
-print ("Top 5 Films")
-print(most_rated)
+result=[]
+for i,v in sort[:5].iterrows():
+    result.append((v['movieId'],-1))
+
+print(result)
